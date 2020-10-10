685f6a6122d753fbe8857a92a82befae156075e0
Josiah Chan <horming8@mbp-jiraiya.lan>
2020-10-10 16:06:02 -0700

    added index.html and controller for pet clinit

---
+++ pet-clinic-web/src/main/java/demo/springframework/petclinicweb/controller/IndexController.java
@@ -0,0 +1,13 @@
+package demo.springframework.petclinicweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(value = { "", "/", "index", "index.html" })
+    public String index() {
+        return "index";
+    }
+}
