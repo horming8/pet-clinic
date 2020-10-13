23e769d4aa96e6d7ad3948a32ea782024ad0c1dc
Josiah Chan <horming8@mbp-jiraiya.lan>
2020-10-13 11:37:17 -0700

    listing owner data on owner index page

--- pet-clinic-web/src/main/java/demo/springframework/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/demo/springframework/petclinic/controller/OwnerController.java
@@ -1,15 +1,25 @@
 package demo.springframework.petclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import demo.springframework.petclinic.service.OwnerService;
 
 @RequestMapping("/owner")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping(value = { "", "/", "/index", "/index.html" })
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owner/index";
     }
 }
