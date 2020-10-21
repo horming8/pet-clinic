d3604033b31cfd2e0852805e947b4531c6489fda
Josiah Chan <horming8@mbp-jiraiya.lan>
2020-10-20 17:05:48 -0700

    modify vet save method to generate id for specialities

--- pet-clinic-data/src/main/java/demo/springframework/petclinic/service/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/demo/springframework/petclinic/service/map/VetServiceMap.java
@@ -5,10 +5,17 @@ import java.util.Set;
 import org.springframework.stereotype.Service;
 
 import demo.springframework.petclinic.model.Vet;
+import demo.springframework.petclinic.service.SpecialityService;
 import demo.springframework.petclinic.service.VetService;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialityService specialityService;
+
+    public VetServiceMap(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
 
     @Override
     public Set<Vet> findAll() {
@@ -22,6 +29,16 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet item) {
+        if (item == null) {
+            return null;
+        }
+        if (item.getSpecialities() != null) {
+            item.getSpecialities().forEach(speciality -> {
+                if (speciality.getId() == null) {
+                    speciality.setId(specialityService.save(speciality).getId());
+                }
+            });
+        }
         return super.save(item);
     }
 
